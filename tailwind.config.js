f553ddb16441646b82841bc3c49a0c6dc731518c
muhammed-mohsen <41347166+muhammed-mohsen@users.noreply.github.com>
2020-11-22 00:16:20 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        yellow: { primary: "#f26723" },
+        gray: {
+          primary: "#f3f3f3",
+        },
+        green: {
+          primary: "#75af2c",
+        },
+      },
+      spacing: {
+        xxlg: "27rem",
+        xlg: "25rem",
+        md: "1.62rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
